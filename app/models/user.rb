602f74e689f259199b83bcbd6ae0b46eb5beda92
Adrian Perez <adrian1804perez@gmail.com>
2024-07-11 14:55:39 -0700

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :user_name, presence: true, length: {
+    in: 5..15,
+    too_short: "too short (minimum is %{count} characters)",
+    too_long: "too long (maximum is %{count} of characters"
+  }
+end
