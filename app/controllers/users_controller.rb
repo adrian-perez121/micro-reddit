37fc82bd1e1178892525a14345a1b0a6321f62bb
Adrian Perez <adrian1804perez@gmail.com>
2024-07-12 14:58:14 -0700

    Add edit functionality for usernames and some refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
