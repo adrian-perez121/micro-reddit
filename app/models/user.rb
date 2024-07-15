d9e833bb8f4d4487c64e7d0c0353b449f7d57d82
Adrian Perez <adrian1804perez@gmail.com>
2024-07-14 17:40:15 -0700

    Changes to association between user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :user_name, presence: true, length: {
     in: 5..15,
     too_short: "too short (minimum is %{count} characters)",
     too_long: "too long (maximum is %{count} of characters"
-  }
+  }, uniqueness: true
 end
