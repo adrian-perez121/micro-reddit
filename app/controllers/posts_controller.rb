5e3ef093edae43e41ddf134412156a4fd03261ec
Adrian Perez <adrian1804perez@gmail.com>
2024-07-14 19:32:11 -0700

    Add create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,21 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.build
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
