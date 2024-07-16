0e9134527c4cc000d7e2bd48bb5133349c79ace6
Adrian Perez <adrian1804perez@gmail.com>
2024-07-16 11:09:05 -0700

    Add Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 20 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
   validates :user_name, presence: true, length: {
     in: 5..15,
     too_short: "too short (minimum is %{count} characters)",
